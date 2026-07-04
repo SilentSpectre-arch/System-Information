e7261ffa9fdc2f2b4203390394e5fcd8b9f83245
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-07-04 20:22:11 +0330

    Initial commit

---
+++ script.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "=== System Information ==="
+echo
+
+echo "CPU Info"
+echo 
+lscpu | grep "Model name" | xargs
+echo
+lscpu | grep Architecture | xargs
+echo
+echo "Physical Cores: $(lscpu | grep "Core(s) per socket:" | cut -d: -f2 | xargs)"
+echo
+echo "Logical Cores: $(nproc | xargs)"
+echo "-----"
+echo
+echo "--RAM status"
+free -h
+echo "-----"
+echo
+echo "--Swap status"
+swapon
+echo "-----"
+echo
+echo "--Disk status"
+df -h
+echo "-----"
+echo
+echo "--Uptime status"
+echo "Uptime: $(uptime | cut -d, -f1 | xargs)"
+echo
+echo "Load average: $(uptime | awk -F'load average:' '{print $2}' | xargs)"
+echo "-----"
+echo
+echo "--Network Info"
+echo "Ip Address: $(hostname -I)"
